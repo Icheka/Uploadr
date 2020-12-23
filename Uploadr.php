9a112c35164a981cb22d0cb50f9c1cb8dae23def
Icheka Ozuru <rhemafortune@gmail.com>
2020-12-23 23:54:15 +0100

    Implemented a slight refactoring of the 'single_upload' method.

--- Uploadr.php
+++ Uploadr.php
@@ -10,8 +10,8 @@ class Uploadr extends CI_Model {
 	'upload_path' => './uploads/',
 	'allowed_types' => 'jpg|png|jpeg|svg|txt|wepg|csv|gif')) {
 		
-		if ((!isset($config['upload_path'])) or (!isset($config['allowed_types'])) {
-			$data['status'] = "Config array must contain <upload_path> and <allowed_types>";
+		if ((!isset($config['upload_path'])) or (!isset($config['allowed_types']))) {
+			$data['status'] = "Config array must contain [upload_path] and [allowed_types]";
 			return $data;
 		}
 	
@@ -21,7 +21,7 @@ class Uploadr extends CI_Model {
 			$data = $this->upload->data();
 			$data['status'] = 'Successful';
 		} else {
-		`	$data = array("error_message" => $this->upload->display_errors(), "status" => "Failed");
+			$data = array("error_message" => $this->upload->display_errors(), "status" => "Failed");
 		}
 		
 		return $data;
